a4e2f2cae07e7865e9bc2dc88ce962174b087a45
Mayank <mayankr20027@gmail.com>
2023-10-21 09:26:53 +0530

    Commit 2, Home Screen UI Finished

--- app/src/main/java/sih/binarylifters/serenitysync/activities/HomeActivity.kt
+++ app/src/main/java/sih/binarylifters/serenitysync/activities/HomeActivity.kt
@@ -1,20 +1,88 @@
 package sih.binarylifters.serenitysync.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.Firebase
+import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import sih.binarylifters.serenitysync.R
+import sih.binarylifters.serenitysync.constants.Constants
+import sih.binarylifters.serenitysync.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private var binding: ActivityHomeBinding? = null
 
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         auth = FirebaseAuth.getInstance()
-        //auth.signOut()
+
+        setupToolbar()
+
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding?.tbHome)
+        supportActionBar?.title = "Hello " + Constants.DISPLAY_NAME
+        binding?.tbHome?.overflowIcon?.setTint(resources.getColor(android.R.color.black, theme))
+        binding?.tbHome?.setTitleTextColor(resources.getColor(android.R.color.black, theme))
+        val maxDisplayNameLength = 20
+
+        val displayName = Constants.DISPLAY_NAME
+        var truncatedDisplayName = if (displayName.length > maxDisplayNameLength) {
+            displayName.substring(0, maxDisplayNameLength - 3) + "..."
+        } else {
+            displayName
+        }
+
+        if (displayName.isEmpty()) {
+            truncatedDisplayName = "Guest"
+        }
+
+        supportActionBar?.title = "Welcome $truncatedDisplayName!"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.about_us -> {
+
+            }
+            R.id.help -> {
+
+            }
+            R.id.sign_out -> {
+                if(Constants.IS_GUEST) {
+                    val intent = Intent(this@HomeActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else {
+                    Constants.IS_GUEST = true
+                    Constants.DISPLAY_NAME = ""
+                    Constants.EMAIL_ID = ""
+                    auth.signOut()
+                    val intent = Intent(this@HomeActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
