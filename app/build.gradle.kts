cb8b69e7272498db42f6a735ebe0c579fc57ec21
mayankr1307 <mayankr20027@gmail.com>
2023-10-30 16:28:29 +0530

    Commit 3, Changed machine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
